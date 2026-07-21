cf3d7a09abe6756595e999c4bcda989575b076ef
magnus <githubstuff07@pm.me>
2026-07-22 00:55:20 +0200

    feat: add getSnapshot fun to SnapshotViewModel.kt WIP

--- app/src/main/java/de/muenchen/appcenter/signalo/SnapshotDisplayFragment.kt
+++ app/src/main/java/de/muenchen/appcenter/signalo/SnapshotDisplayFragment.kt
@@ -5,23 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.fragment.navArgs
+import de.muenchen.appcenter.signalo.databinding.FragmentSnapshotDisplayBinding
 
 class SnapshotDisplayFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private lateinit var _binding: FragmentSnapshotDisplayBinding
+    val selectedSnapshotID = navArgs<SnapshotDisplayFragmentArgs>().value.creationDate
+    private val snapshotViewModel: SnapshotViewModel by viewModels { factory }
+    private val factory = viewModelFactory {
+        initializer {
+            val store = requireContext().applicationContext.snapshotDataStore
+            SnapshotViewModel(SnapshotRepository(store))
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        val selectedSnapshotID = navArgs<SnapshotDisplayFragmentArgs>().value.creationDate
-        return inflater.inflate(R.layout.fragment_snapshot_display, container, false)
+        _binding = FragmentSnapshotDisplayBinding.inflate(inflater, container, false)
+        return _binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        snapshotViewModel.getSnapshot(selectedSnapshotID)
+        snapshotViewModel.currentSnapshot.observe(viewLifecycleOwner) {
+            DisplayValues()
+        }
+    }
+
+    private fun DisplayValues() {
+
+
+    }
 }
--- app/src/main/java/de/muenchen/appcenter/signalo/SnapshotViewModel.kt
+++ app/src/main/java/de/muenchen/appcenter/signalo/SnapshotViewModel.kt
@@ -1,10 +1,24 @@
 package de.muenchen.appcenter.signalo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class SnapshotViewModel(private val repository: SnapshotRepository) : ViewModel() {
+    var currentSnapshot: MutableLiveData<Snapshot?> = MutableLiveData(null)
     val snapshots: Flow<List<Snapshot>> = repository.snapshots
 
-    
+    fun getSnapshot(id: Long) {
+        viewModelScope.launch {
+            val allSnapshots = repository.snapshots.first()
+            val selectedSnapshot = allSnapshots.find { it.creationDate == id }
+            currentSnapshot.postValue(selectedSnapshot)
+
+        }
+    }
+
 }
+
