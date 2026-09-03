18e2cc4e68278fab082435f3570da1ed56239c35
magnus <magnusvonwachter@gmail.com>
2026-09-04 00:31:26 +0200

    fix: Encrpytion Constants.kt

--- app/src/main/java/de/muenchen/appcenter/signalo/utils/Constants.kt
+++ app/src/main/java/de/muenchen/appcenter/signalo/utils/Constants.kt
@@ -44,7 +44,7 @@ class Constants {
 
         // ENCRYPTION CONSTANTS - DO NOT CHANGE AFTER RELEASE!
         // Changing any value makes all existing data permanently unreadable
-        const val MASTER_KEY_URI = "android-keystore://signalo_snapshot_master_key"
+        const val MASTER_KEY_ALIAS = "signalo_snapshot_master_key"
         const val KEYSET_NAME = "signalo_snapshot_keyset"
         const val KEYSET_PREFS_FILE = "signalo_snapshot_keyset_prefs"
         const val ASSOCIATED_DATA = "signalo-snapshot-v1"
