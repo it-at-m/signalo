fe831b4820e637a3498d965cf5641ebc721cd667
magnus <githubstuff07@pm.me>
2026-09-13 01:35:07 +0200

    feat: encrypt snapshots store with AEAD

--- app/src/main/java/de/muenchen/appcenter/signalo/SnapshotSerializer.kt
+++ app/src/main/java/de/muenchen/appcenter/signalo/SnapshotSerializer.kt
@@ -2,6 +2,7 @@ package de.muenchen.appcenter.signalo
 
 import androidx.datastore.core.IOException
 import androidx.datastore.core.Serializer
+import de.muenchen.appcenter.signalo.utils.Constants
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import timber.log.Timber
@@ -11,11 +12,19 @@ import java.io.OutputStream
 object SnapshotSerializer : Serializer<SnapshotContainer> {
     override suspend fun readFrom(input: InputStream): SnapshotContainer {
         return try {
+            val ciphertext = input.readBytes()
+            // don't try to decrypt an empty value
+            if (ciphertext.isEmpty()) {
+                return defaultValue
+            }
+            val plaintext =
+                CryptoProvider.aead.decrypt(ciphertext, Constants.ASSOCIATED_DATA.toByteArray())
             Json.decodeFromString(
                 deserializer = SnapshotContainer.serializer(),
-                string = input.readBytes().decodeToString()
+                string = plaintext.decodeToString()
             )
         } catch (e: SerializationException) {
+            e.printStackTrace()
             Timber.d("Snapshot Read Failed: SerializationException Erorr: " + e.printStackTrace())
             defaultValue
         } catch (e: IOException) {
@@ -26,11 +35,10 @@ object SnapshotSerializer : Serializer<SnapshotContainer> {
     }
 
     override suspend fun writeTo(t: SnapshotContainer, output: OutputStream) {
-        output.write(
-            Json.encodeToString(
-                serializer = SnapshotContainer.serializer(), value = t
-            ).encodeToByteArray()
-        )
+        val plaintext = Json.encodeToString(SnapshotContainer.serializer(), t).toByteArray()
+        val ciphertext =
+            CryptoProvider.aead.encrypt(plaintext, Constants.ASSOCIATED_DATA.toByteArray())
+        output.write(ciphertext)
     }
 
     override val defaultValue: SnapshotContainer
